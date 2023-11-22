76a2071865cadf0164187638f5b533e47c2dc413
user <watanabe.miyu@technopro.com>
2023-11-22 10:06:15 +0900

    eventrepositoryに記述追加

--- src/main/java/com/example/repository/EventRepository.java
+++ src/main/java/com/example/repository/EventRepository.java
@@ -1,5 +1,7 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.example.entity.Event;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
+
+	List<Event> findEventsForMonth(int year, int month);
 
 }
